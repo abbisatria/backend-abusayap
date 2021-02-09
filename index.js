0d913dc4456df2ecfa653a2457bf2b215bd719e6
ridhomujizat <ridhomujizat@gmail.com>
2021-02-09 22:48:27 +0700

    boilerplate

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config()
+const { APP_PORT } = process.env
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+app.use(cors('*'))
+
+app.use('/uploads', express.static('uploads'))
+
+// Put Routes in here
+
+// Test App
+app.get('/', (req, res) => {
+  const data = {
+    success: true,
+    message: 'Backend is running wel'
+  }
+  res.send(data)
+})
+
+// Not Found Api
+app.get('*', (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: 'API Not Found!'
+  })
+})
+
+app.listen(APP_PORT, () => {
+  console.log(`App is running on port ${APP_PORT}`)
+})
