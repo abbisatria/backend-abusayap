2f91967e6e1f5d392b9a4f3f43e5e76d3c8ca83e
ridhomujizat <ridhomujizat@gmail.com>
2021-02-11 10:39:57 +0700

    get contact endpoint

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use('/uploads', express.static('uploads'))
 app.use('/auth', require('./src/routes/auth'))
 app.use('/user', require('./src/routes/user'))
 app.use('/transaction', require('./src/routes/transaction'))
+app.use('/contact', require('./src/routes/contact'))
 
 // Test App
 app.get('/', (req, res) => {
--- src/models/users.js
+++ src/models/users.js
@@ -42,3 +42,47 @@ exports.updateUser = (id, data) => {
     console.log(query.sql)
   })
 }
+
+exports.getAllContactByCondition = (id, cond) => {
+  return new Promise((resolve, reject) => {
+    const query = db.query(`
+    SELECT id, CONCAT(firstname, ' ', lastname) AS name, email, phoneNumber, picture 
+    FROM users
+    WHERE CONCAT(firstname, ' ', lastname) LIKE "%${cond.search}%" AND id NOT IN (${id})
+    ORDER BY ${cond.sort} ${cond.order}
+    LIMIT ${cond.limit} OFFSET ${cond.offset}
+    `, (err, res, field) => {
+      if (err) reject(err)
+      resolve(res)
+    })
+    console.log(query.sql)
+  })
+}
+
+exports.getCountContactByCondition = (id, cond) => {
+  return new Promise((resolve, reject) => {
+    const query = db.query(`
+    SELECT COUNT(id) as totalData 
+    FROM users 
+    WHERE CONCAT(firstname, ' ', lastname) LIKE "%${cond.search}%"  AND id NOT IN (${id})
+    `, (err, res, field) => {
+      if (err) reject(err)
+      resolve(res)
+    })
+    console.log(query.sql)
+  })
+}
+
+exports.getCountContact = (id, cond) => {
+  return new Promise((resolve, reject) => {
+    const query = db.query(`
+    SELECT COUNT(id) as totalData 
+    FROM users 
+    WHERE id NOT IN (${id})
+    `, (err, res, field) => {
+      if (err) reject(err)
+      resolve(res)
+    })
+    console.log(query.sql)
+  })
+}
